7325a812a301a571c4759321ed3bb0e4c3d9e3b3
Merimka <usamerimka2110icloud.com>
2023-04-01 13:38:42 -0400

    First Commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+
+print('Hello World')
+
+print("It's a lovely")
+
+print('He said "It\'s a lovely day"')
+
+print("He said \"It\'s a lovely day\"")
+
+print ("\\python\\")
+print ('\python\\')
